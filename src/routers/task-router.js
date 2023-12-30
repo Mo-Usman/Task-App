86b6b2aa4741f37bdd1454213661b6ec9dce4139
Muhammad Usman <muhammadusmanishaq123@gmail.com>
2023-12-30 10:59:14 +0500

    Add pagination and data filtering

--- src/routers/task-router.js
+++ src/routers/task-router.js
@@ -24,15 +24,26 @@ router.post('/tasks', auth, async (req, res) => {
 router.get('/tasks', auth, async (req, res) => {
 
     const match = {}
+    const sort = {}
 
     if(req.query.status) {
         match.status = req.query.status === 'true'
     }
 
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
             await req.user.populate({
             path: 'tasks',
-            match: match
+            match: match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort: sort
+            }
         })
         res.send(req.user.tasks)
     } catch (e) {
